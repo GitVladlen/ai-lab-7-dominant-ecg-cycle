7a99cdf3197abe961b5427c3f1731898a58cfef9
Vladlen Fedorenko <vladlen.fedorenko@gmail.com>
2020-12-07 10:30:07 +0200

    wip

--- main.py
+++ main.py
@@ -13,47 +13,62 @@ def read_from_file(file_name):
 
 
 def plotting(p1, p2, p3, p4, x1, x2, x3, x4):
-    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(16, 6))
-    # plt.subplots_adjust(wspace=0.2, hspace=0.4)
     p1.plot(x1, 'r')
-    p1.set_title('1')
+    p1.set_title('ЕКГ 1')
+
     p2.plot(x2, 'b')
-    p2.set_title('2')
+    p2.set_title('ЕКГ 2')
+
     p3.plot(x3, 'g')
-    p3.set_title('3')
+    p3.set_title('ЕКГ 3')
+
     p4.plot(x4, 'y')
-    p4.set_title('4')
-    # plt.show()
+    p4.set_title('ЕКГ 4')
 
 
-def plotting_dxdz(p5, p6, x1, t1, x2, t2, x3, t3, x4, t4):
-    # fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
+def plotting_dxdz(p5, p6, x1, t1, x2, t2, x3, t3, x4, t4, dominant):
     p5.plot(x1, t1, 'r')
     p5.plot(x2, t2, 'b')
     p5.plot(x3, t3, 'g')
     p5.plot(x4, t4, 'y')
-    p5.set_title('Phases graphic')
-    p6.plot(x1, t1, 'r', linewidth=9)
-    p6.plot(x2, t2, 'b')
-    p6.plot(x3, t3, 'g')
-    p6.plot(x4, t4, 'y')
-    p6.set_title('Phases graphic with dominant sequence')
-    # plt.show()
+    p5.set_title('Фазові портрети')
+
+    if dominant == 1:
+        p6.plot(x1, t1, 'r', linewidth=9)
+    else:
+        p6.plot(x1, t1, 'r')
+
+    if dominant == 2:
+        p6.plot(x2, t2, 'b', linewidth=9)
+    else:
+        p6.plot(x2, t2, 'b')
+
+    if dominant == 3:
+        p6.plot(x3, t3, 'g', linewidth=9)
+    else:
+        p6.plot(x3, t3, 'g')
+
+    if dominant == 4:
+        p6.plot(x4, t4, 'y', linewidth=9)
+    else:
+        p6.plot(x4, t4, 'y')
+    p6.set_title('З домінантим сигналом')
 
 
-def plot(p1, p2, p3, p4, p5, p6, x, norm_x, der_norm_x):
-    plotting(
-        p1, p2, p3, p4
-        , x[0], x[1], x[2], x[3]
-    )
-    # plotting(x[0], x[1], x[2], x[3])
-    plotting_dxdz(
-        p5, p6,
-        der_norm_x[0], norm_x[0],
-        der_norm_x[1], norm_x[1],
-        der_norm_x[2], norm_x[2],
-        der_norm_x[3], norm_x[3]
-    )
+# def plot(p1, p2, p3, p4, p5, p6, x, norm_x, der_norm_x):
+#     plotting(
+#         p1, p2, p3, p4
+#         , x[0], x[1], x[2], x[3]
+#     )
+#     # plotting(x[0], x[1], x[2], x[3])
+#     plotting_dxdz(
+#         p5, p6,
+#         der_norm_x[0], norm_x[0],
+#         der_norm_x[1], norm_x[1],
+#         der_norm_x[2], norm_x[2],
+#         der_norm_x[3], norm_x[3],
+#         1
+#     )
 
 
 def derivat(x):
@@ -85,44 +100,68 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 root = tkinter.Tk()
+# root.geometry("1200x300")
 root.wm_title("Визначення опорного циклу ЕКГ")
-
-fig = Figure(figsize=(10, 6), dpi=100)
-
-fig.subplots_adjust(wspace=0, hspace=0.4)
-
-sub_plot_1 = fig.add_subplot(321)
-sub_plot_2 = fig.add_subplot(322)
-sub_plot_3 = fig.add_subplot(323)
-sub_plot_4 = fig.add_subplot(324)
-sub_plot_5 = fig.add_subplot(325)
-sub_plot_6 = fig.add_subplot(326)
-
-# sub_plot_1.set_title('Original Signal')
-# sub_plot_2.set_title('Phases on dz/dt')
-# sub_plot_3.set_title('Phases on z(t - pause)')
-
-plot(
-    sub_plot_1, sub_plot_2,
-    sub_plot_3, sub_plot_4,
-    sub_plot_5, sub_plot_6,
-    x, norm_x, der_norm_x
-)
-
-fig.subplots_adjust(wspace=0.2, hspace=0.4)
-
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
-
-canvas.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
 
 R = np.zeros((4, 4))
 for i in range(4):
     for j in range(4):
         R[i, j] = round(directed_hausdorff(norm_x[i], norm_x[j])[0], 3)
 
-print('\n\nDistance Hausdorf:\n\n' + str(R) + '\n\n')
-print(np.argmin(R.sum(axis=1)) + 1)
+text = '\n\nDistance Hausdorf:\n\n' + str(R) + '\n\n' + str(np.argmin(R.sum(axis=1)) + 1)
+print(text)
+
+# Fig 1
+fig1 = Figure(figsize=(15, 3), dpi=100)
+
+fig1.subplots_adjust(wspace=0, hspace=1)
+
+sub_plot_1 = fig1.add_subplot(141)
+sub_plot_2 = fig1.add_subplot(142)
+sub_plot_3 = fig1.add_subplot(143)
+sub_plot_4 = fig1.add_subplot(144)
+
+plotting(
+    sub_plot_1, sub_plot_2, sub_plot_3, sub_plot_4
+    , x[0], x[1], x[2], x[3]
+)
+
+canvas1 = FigureCanvasTkAgg(fig1, master=root)
+canvas1.draw()
+
+canvas1.get_tk_widget().pack(side=tkinter.TOP,
+                             fill=tkinter.X,
+                             # expand=1
+                             )
+
+
+
+# Fig 2
+fig2 = Figure(figsize=(4, 4), dpi=100)
+
+sub_plot_5 = fig2.add_subplot(121)
+sub_plot_6 = fig2.add_subplot(122)
+
+plotting_dxdz(
+    sub_plot_5, sub_plot_6,
+    der_norm_x[0], norm_x[0],
+    der_norm_x[1], norm_x[1],
+    der_norm_x[2], norm_x[2],
+    der_norm_x[3], norm_x[3],
+    1
+)
+
+fig2.subplots_adjust(wspace=0.5, hspace=1)
+
+canvas2 = FigureCanvasTkAgg(fig2, master=root)
+canvas2.draw()
+
+canvas2.get_tk_widget().pack(side=tkinter.BOTTOM,
+                             fill=tkinter.X,
+                             # expand=1
+                             )
+
+tkinter.Label(root, text=text).pack(side=tkinter.RIGHT, fill=tkinter.Y)
 
 root.mainloop()
 
