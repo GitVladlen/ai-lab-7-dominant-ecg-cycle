859573596e773f564fa0bb0b447a32ed4dc9fde4
Vladlen Fedorenko <vladlen.fedorenko@gmail.com>
2020-12-11 10:02:38 +0200

    code cleanup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import numpy as np
+import math
 import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from prettytable import PrettyTable
 
-import numpy as np
-from scipy.spatial.distance import directed_hausdorff
-import math
 
 def read_from_file(file_name):
     f = open(file_name, 'r')
@@ -58,7 +58,6 @@ def plotting_dxdz_dominant(p6, x1, t1, x2, t2, x3, t3, x4, t4, dominant):
     else:
         p6.plot(x3, t3, 'g')
 
-
     p6.set_title('З домінантим сигналом')
 
 
@@ -72,7 +71,7 @@ def derivat(x):
         d = x[i - 1] if i - 1 >= 0 else x[0]
         e = x[i - 2] if i - 2 >= 0 else x[0]
         g = x[i - 3] if i - 3 >= 0 else x[0]
-        z_i = (1/60) * (a -9*b +45*c -45*d +9*e -g)
+        z_i = (1/60) * (a - 9*b + 45*c - 45*d + 9*e - g)
         z.append(z_i)
     return np.array(z)
 
@@ -86,8 +85,10 @@ x = np.array([read_from_file(str(i + 1) + '.txt') for i in range(4)])
 norm_x = np.array([normalize(x[i]).reshape(len(x[i]), 1) for i in range(len(x))])
 der_norm_x = np.array([normalize(derivat(x[i])) for i in range(len(x))])
 
+
 def calc_euclidean_distance(x1, y1, x2, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
 
 def calc_hausdorff_distance(u_x_arr, u_y_arr, v_x_arr, v_y_arr):
     min_euclidean_distances = []
@@ -116,18 +117,14 @@ def calc_hausdorff_distance(u_x_arr, u_y_arr, v_x_arr, v_y_arr):
 
 r = round(calc_hausdorff_distance(norm_x[0], der_norm_x[0], norm_x[0], der_norm_x[0]), 3)
 
-# calc distances
+# calc matrix and
 R = np.zeros((4, 4))
 for i in range(4):
     for j in range(4):
-        # R[i, j] = round(directed_hausdorff(norm_x[i], norm_x[j])[0], 3)
         R[i, j] = round(calc_hausdorff_distance(norm_x[i], der_norm_x[i], norm_x[j], der_norm_x[j]), 3)
 
+# calc dominant as matrix index
 index = np.argmin(R.sum(axis=1)) + 1
-
-# prepare table text
-text = '\n\nDistance Hausdorf:\n\n' + str(R) + '\n\n' + str(index)
-print(text)
 
 # visualization
 root = tkinter.Tk()
@@ -144,61 +141,42 @@ sub_plot_2 = fig1.add_subplot(142)
 sub_plot_3 = fig1.add_subplot(143)
 sub_plot_4 = fig1.add_subplot(144)
 
-plotting(
-    sub_plot_1, sub_plot_2, sub_plot_3, sub_plot_4
-    , x[0], x[1], x[2], x[3]
-)
+plotting(sub_plot_1, sub_plot_2, sub_plot_3, sub_plot_4,
+         x[0], x[1], x[2], x[3])
 
 canvas1 = FigureCanvasTkAgg(fig1, master=root)
 canvas1.draw()
 
-canvas1.get_tk_widget().pack(side=tkinter.TOP,
-                             fill=tkinter.BOTH,
-                             expand=1
-                             )
+canvas1.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 # Phase figs panel
 panel = tkinter.PanedWindow(root)
 panel.configure(background='white')
-panel.pack(side=tkinter.BOTTOM,
-           fill=tkinter.BOTH,
-           expand=1
-           )
+panel.pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
 
 # Fig 2
 fig2 = Figure(figsize=(4, 4), dpi=100)
 
 sub_plot_5 = fig2.add_subplot(111)
 
-plotting_dxdz(
-    sub_plot_5,
-    der_norm_x[0], norm_x[0],
-    der_norm_x[1], norm_x[1],
-    der_norm_x[2], norm_x[2],
-    der_norm_x[3], norm_x[3]
-)
+plotting_dxdz(sub_plot_5,
+              der_norm_x[0], norm_x[0],
+              der_norm_x[1], norm_x[1],
+              der_norm_x[2], norm_x[2],
+              der_norm_x[3], norm_x[3])
 
 fig2.subplots_adjust(wspace=0.5, hspace=1)
 
 canvas2 = FigureCanvasTkAgg(fig2, master=panel)
 canvas2.draw()
 
-canvas2.get_tk_widget().pack(side=tkinter.LEFT,
-                             fill=tkinter.BOTH,
-                             expand=1
-                             )
+canvas2.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
 
 # Table
-from prettytable import PrettyTable
-
 table = PrettyTable()
-
 table.field_names = ["Rij", "1", "2", "3", "4", "Sum"]
 
 for i, r in enumerate(R, 1):
-    # row = []
-    # row.append(str(i))
-    # row += r
     ind = str(i)
     if index == i:
         ind = "[" + str(i) + "]"
@@ -207,37 +185,28 @@ for i, r in enumerate(R, 1):
     for j, c in enumerate(r, 1):
         a.append(str(c))
         sum += c
-    a.append(str(round(sum,3)))
+    a.append(str(round(sum, 3)))
     table.add_row(a)
-
-
 
 # Fig 3
 fig3 = Figure(figsize=(4, 4), dpi=100)
 
 sub_plot_6 = fig3.add_subplot(111)
 
-plotting_dxdz_dominant(
-    sub_plot_6,
-    der_norm_x[0], norm_x[0],
-    der_norm_x[1], norm_x[1],
-    der_norm_x[2], norm_x[2],
-    der_norm_x[3], norm_x[3],
-    index
-)
+plotting_dxdz_dominant(sub_plot_6,
+                       der_norm_x[0], norm_x[0],
+                       der_norm_x[1], norm_x[1],
+                       der_norm_x[2], norm_x[2],
+                       der_norm_x[3], norm_x[3],
+                       index)
 
 fig3.subplots_adjust(wspace=0.5, hspace=1)
 
 canvas3 = FigureCanvasTkAgg(fig3, master=panel)
 canvas3.draw()
 
-canvas3.get_tk_widget().pack(side=tkinter.RIGHT,
-                             fill=tkinter.BOTH,
-                             expand=1
-                             )
+canvas3.get_tk_widget().pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
 
 tkinter.Label(panel, background='white', text=str(table), font="Consolas 10").pack(side=tkinter.RIGHT)
 
 root.mainloop()
-
-
